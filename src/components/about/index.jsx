7a2154bdfeb874b3ce850a534a9794d44379ae6e
Clinton Rocha <clintongoncalvesrocha@gmail.com>
2023-01-23 10:38:36 -0300

    about page

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,13 +1,34 @@
 import HeadConfig from "../../hooks/HeadConfig";
+import contato from "../../../public/contato.jpg";
+import styles from "./styles.module.scss";
+export function About() {
+  return (
+    <>
+      <HeadConfig
+        title="Ranek | Sobre"
+        description="pagina com informação da loja Ranek"
+      />
 
-export function About(){
-    return (
-        <>
-              <HeadConfig title="Ranek | Sobre" description="pagina com informação da loja Ranek" />
-
-            <section>
-                <h2>sobre</h2>
-            </section>
-        </>
-    )
+      <section className="container animeLeft">
+        <div className={styles.sobre}>
+          <img src={contato} alt="a woman " />
+          <div>
+            <h2>Lorem ipsum dolor sit amet.</h2>
+            <br />
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Provident cupiditate dolorem unde aliquid temporibus ipsum animi
+              beatae quis consequatur, illum vel nobis exercitationem quos?
+              Dignissimos quas in corrupti excepturi repellat.
+            </p>
+            <br />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas,
+              voluptatem.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
